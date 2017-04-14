0146816f97715704d49714698dbcc60b9975036e
ljw <13163235957@163.com>
2017-04-14 19:26:10 +0800

    在测测logback

--- src/main/java/com/hyschool/common/controller/CommonController.java
+++ src/main/java/com/hyschool/common/controller/CommonController.java
@@ -1,5 +1,8 @@
 package com.hyschool.common.controller;
 
+import com.hyschool.vip.bean.Vip;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +14,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CommonController {
 
+    private static Logger logger = LoggerFactory.getLogger(CommonController.class);
+
     @RequestMapping("/about")
     public String about(){
         return "common/about";
+    }
+
+    @RequestMapping("/log")
+    public String testLog(){
+        logger.debug("debug test");
+        logger.info("info test");
+        logger.warn("warn test");
+        logger.error("error test");
+        Vip vip = new Vip();
+        System.out.println(vip.getName().indexOf(5));
+        return "redirect:/";
     }
 
 }
